5cd352e1a4e59d32f8d97d6b27dd3179e029d56a
Troy <wilson.troy.anthony@gmail.com>
2022-10-31 21:28:09 -0400

    Started

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ToDo } from './ToDo';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ToDo';
+  title = 'ToDo List';
+  todo: ToDo[] = [
+    { task: 'Learn Angular', completed: false },
+    { task: 'Learn TypeScript', completed: false },
+    { task: 'Learn JavaScript', completed: true },
+    { task: 'Learn HTML', completed: false },
+    { task: 'Learn CSS', completed: false },
+  ];
+
+
+  addTask(formParam: NgForm): void{
+    if(formParam.value.task !== ''){
+      let newTask: ToDo = {
+        task: formParam.value.task,
+        completed: false
+      };
+      this.todo.push(newTask);    
+  }
+}
+
+completeTask(index: number): void{
+    this.todo[index].completed = true;
+  }
 }
